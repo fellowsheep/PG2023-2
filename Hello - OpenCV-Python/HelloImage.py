22488ac803a7f0ed326b6e3e29d1546ec5964aa8
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-10-30 22:08:06 -0300

    Update HelloImage.py

--- Hello - OpenCV-Python/HelloImage.py
+++ Hello - OpenCV-Python/HelloImage.py
@@ -4,16 +4,17 @@ import cv2 as cv
 img = cv.imread('baboon.png') #original
 img2 = img.copy() #copia para img modificada
 img3 = img.copy()
+img10 = img.copy()
 
 img4 = img.copy()
-corModificadora = [0, 0, 255]
+corModificadora = [255, 0, 0]
 
 img5 = cv.imread('bolinhas.png') #original
 img6 = img5.copy()
 
 img7 = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 img8 = img7.copy()
-k = 200
+k = 150
 
 img9 = img.copy()
 
@@ -34,6 +35,11 @@ for i in range(img.shape[0]): #percorre linhas
 		img3.itemset((i,j,1),mediaPond) # canal G
 		img3.itemset((i,j,2),mediaPond) # canal R
 
+		B = img10.item(i,j,0)
+		img10.itemset((i,j,0),B) # canal B
+		img10.itemset((i,j,1),B) # canal G
+		img10.itemset((i,j,2),B) # canal R
+
 		B = img.item(i,j,0) | corModificadora[0]
 		G = img.item(i,j,1) | corModificadora[1]
 		R = img.item(i,j,2) | corModificadora[2]
@@ -41,9 +47,9 @@ for i in range(img.shape[0]): #percorre linhas
 		img4.itemset((i,j,1),G) # canal G
 		img4.itemset((i,j,2),R) # canal R
 
-		img6.itemset((i,j,0),img5.item(i,j,0)^255) # canal B
-		img6.itemset((i,j,1),img5.item(i,j,1)^255) # canal G
-		img6.itemset((i,j,2),img5.item(i,j,2)^255) # canal R
+		img6.itemset((i,j,0),img6.item(i,j,0)^255) # canal B
+		img6.itemset((i,j,1),img6.item(i,j,1)^255) # canal G
+		img6.itemset((i,j,2),img6.item(i,j,2)^255) # canal R
 
 		if img7.item(i,j) < k:
 			img8.itemset((i,j),0)
@@ -64,17 +70,14 @@ for i in range(img.shape[0]): #percorre linhas
 			img9.itemset((i,j,1),255)
 			img9.itemset((i,j,2),255)
 
-
-
-
-
 cv.imshow("Original",img)
-#cv.imshow("Grayscale - Média Aritmética",img2)
-#cv.imshow("Grayscale - Média Ponderada",img3)
-#cv.imshow("Imagem colorizada",img4)
-#cv.imshow("Imagem invertida",img6)
+# cv.imshow("Grayscale - Média Aritmética",img2)
+# cv.imshow("Grayscale - Média Ponderada",img3)
+# cv.imshow("Grayscale - Apenas um canal",img10)
+cv.imshow("Imagem colorizada",img4)
+cv.imshow("Imagem invertida",img6)
 cv.imshow("Imagem Grayscale - OpenCV",img7)
-#cv.imshow("Imagem Binarizada",img8)
+cv.imshow("Imagem Binarizada",img8)
 cv.imshow("Imagem Color Ramp",img9)
 
 cv.waitKey(0)
